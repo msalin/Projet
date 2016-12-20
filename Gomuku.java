8eec4265d118ccf1aa0d8b7226d8d5b0e834c982
msalin <maesalingre@gmail.com>
2016-12-20 17:17:10 +0100

    Update Gomuku.java

--- Gomuku.java
+++ Gomuku.java
@@ -1,3 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Gomuku extends Jeu {
+  
+  public Gomuku(Joueur j1, Joueur j2, int taille){
+    super(j1, j2, new Plateau(taille));
+    HashMap<String, List<String>> h = new HashMap();
+    List<String> l1 = new ArrayList();
+    l1.add("black");
+    h.put("black", l1);
+    List<String> l2 = new ArrayList();
+    l2.add("white");
+    h.put("white",l2);
+    this.plateau.setCodeCouleur(h);
+  }
 
 }
