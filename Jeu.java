2742db8a737cc4f79b294a5f46f2a4f6b3ce38c8
msalin <maesalingre@gmail.com>
2016-12-20 14:24:15 +0100

    Update Jeu.java

--- Jeu.java
+++ Jeu.java
@@ -1,3 +1,11 @@
 public abstract class Jeu {
-
+  protected Joueur joueur1;
+  protected Joueur joueur2;
+  protected Plateau plateau;
+  
+  public Jeu(Joueur j1, Joueur j2, Plateau p){
+    this.joueur1 = j1;
+    this.joueur2 = j2;
+    this.plateau = p;
+  }
 }
