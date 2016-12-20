a71634c781e733c283ef8dcefb9d2e3360c44b7c
msalin <maesalingre@gmail.com>
2016-12-20 12:58:35 +0100

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -1,3 +1,17 @@
 public abstract class Joueur {
-
+  public final String nom;
+  private int nbPointsCourants;
+  
+  public Joueur(String nom){
+    this.nom = nom;
+    this.nbPointsCourants = 0;
+  }
+  
+  public void reset(){
+    this.nbPointsCourants = 0;
+  }
+  
+  public int getNbPoints(){
+    return this.nbPointsCourants;
+  }
 }
