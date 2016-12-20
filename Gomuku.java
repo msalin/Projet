8b0952c8e9b2df1370058ee95acd3f905f32dcda
msalin <maesalingre@gmail.com>
2016-12-20 23:21:10 +0100

    Update Gomuku.java

--- Gomuku.java
+++ Gomuku.java
@@ -15,5 +15,30 @@ public class Gomuku extends Jeu {
     h.put("white",l2);
     this.plateau.setCodeCouleur(h);
   }
+  
+  public void jouer(){
+    while(this.plateau.existeCasesLibres()){
+      tour(this.joueur1, "black");
+      if (this.plateau.existeCasesLibres()){
+        tour(this.joueur2, "white");
+      }
+    }
+  }
+  
+  private void tour(Joueur j, String couleur){
+    int[] coordonnees = j.getCoup();
+    try {
+      this.plateau.pose(coordonnees[0], coordonnees[1], couleur); 
+      int[] score = this.plateau.getAlign(coordonnees[0], coordonnees[1]);
+      for (int i=0; i<score.length; i++){
+       if (score[i]>=5){
+        j.addScore(1); 
+       }
+      }
+    } catch (CaseOccupeeException e){
+      System.out.println(e);
+      tour(j, couleur);
+    }
+  }
 
 }
