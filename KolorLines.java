4acca1d8dfec126cbf14dcb9132c194326796d57
msalin <maesalingre@gmail.com>
2016-12-20 15:14:14 +0100

    Update KolorLines.java

--- KolorLines.java
+++ KolorLines.java
@@ -1,3 +1,21 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class KolorLines extends Jeu {
+  
+  private List<String> prochainsCoups;
+  
+  public KolorLines(Joueur j1, int taille){
+    super(j1, new JoueurIA(), new Plateau(taille));
+    HashMap<String, List<String>> h = new HashMap();
+    String[] couleurs = new String[] {"black", "white", "blue", "red", "yellow"}
+    for(int i=0; i<5; i++){
+      List<String> l = new ArrayList();
+      l.add(couleurs[i]);
+      l.add("rainbow");
+      h.put(couleurs[i], l);
+    }
+    this.plateau.setCodeCouleur(h);
 
 }
