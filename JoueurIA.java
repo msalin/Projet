7b9cc4d8e5f2782da6a6c39bfb67e6046342f01e
msalin <maesalingre@gmail.com>
2016-12-21 14:47:25 +0100

    Update JoueurIA.java

--- JoueurIA.java
+++ JoueurIA.java
@@ -1,7 +1,16 @@
+import java.util.Random;
 public class JoueurIA extends Joueur{
 
   public JoueurIA(){
     super("Ordinateur");
   }
+  
+  public int[] getCoup(){
+	  Random r = new Random();
+	  int index = r.nextInt(this.coupsPossibles.size());
+	  Plateau.Case c = this.coupsPossibles.get(index);
+	  return new int[] {c.i, c.j};
+  }
 
 }
+
