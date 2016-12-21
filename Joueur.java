28a25db4c1ba35173c38d8451f233ff7ae0ca261
msalin <maesalingre@gmail.com>
2016-12-21 14:49:18 +0100

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -1,14 +1,23 @@
+import java.util.List;
 public abstract class Joueur {
   public final String nom;
   private int nbPointsCourants;
+  protected List<Plateau.Case> coupsPossibles;
   
   public Joueur(String nom){
     this.nom = nom;
     this.nbPointsCourants = 0;
   }
   
+  public void setCoupsPossibles(List<Plateau.Case> cp){
+	  this.coupsPossibles = cp;
+  }
   public void reset(){
     this.nbPointsCourants = 0;
+  }
+  
+  public void addScore(int i){
+	  this.nbPointsCourants += i;
   }
   
   public int getNbPoints(){
